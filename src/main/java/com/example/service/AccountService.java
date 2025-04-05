ac803f5316ef17cfec2de4f978116237b88e412f
Jagadeswar Tanguturi <jagadeswar.tanguturi2147@my.riohondo.edu>
2025-04-05 03:44:41 +0000

    Completed Account

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,48 @@
 package com.example.service;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    public ResponseEntity<Account> registerAccount(Account newAccount){
+        String username = newAccount.getUsername();
+        String password = newAccount.getPassword();
+        Account finalAccount = null;
+        if (username.isEmpty() || password.length()<4){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Optional<Account> optionalAccount = accountRepository.findByUsername(username);
+        if (optionalAccount.isPresent()){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        } else{
+            finalAccount =  accountRepository.save(newAccount);
+        }
+        return new ResponseEntity<>(finalAccount, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Account> loginAccount(Account account){
+        String username = account.getUsername();
+        String password = account.getPassword();
+
+        Optional<Account> optionalAccount = accountRepository.findByUsernameAndPassword(username, password);
+        if (optionalAccount.isPresent()){
+            return new ResponseEntity<>(optionalAccount.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,46 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import com.example.entity.*;
+import com.example.repository.*;
+import java.util.*;
+
+@Service
 public class MessageService {
+
+    private MessageRepository messageRepository;
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository, AccountRepository accountRepository){
+        this.messageRepository = messageRepository;
+        this.accountRepository=accountRepository;
+    }
+
+    public ResponseEntity<Message> createMessage(Message newMessage){
+        String messageText = newMessage.getMessageText();
+        Integer postedBy = newMessage.getPostedBy();
+
+        Message finalMessage=null;
+
+
+        Optional<Account> optionalAccount = accountRepository.findById(postedBy);
+        if (optionalAccount.isPresent()){
+            if (messageText.length()>255 || messageText.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);    
+            }
+            else{
+            finalMessage = messageRepository.save(newMessage);
+            }
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(finalMessage,HttpStatus.OK);
+
+    }
+
 }
